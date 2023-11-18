fd141f18aeca62d501006e9451a4819413bbf2c8
srinu5713 <96185709+srinu5713@users.noreply.github.com>
2023-11-18 21:26:06 +0530

    date error handling

--- AGMS_Modules/Guide_book.py
+++ AGMS_Modules/Guide_book.py
@@ -27,43 +27,49 @@ def do_guide_book(username):
                     option=st.selectbox("Select Guide ID to book: ",list2)
                     d1=st.date_input("Enter the Date: ")
                     s=st.selectbox("Enter your preferred Slot",("1 (9:30 AM to 10:30 AM)","2 (11:00 AM to 12:00 PM)","3 (1:30 PM to 2:30 PM)","4 (3:00 PM to 4:00 PM)","5 (4:30 PM to 5:30 PM)"))
+                    current_date = datetime.now().date()
 
-                    if st.button("Book"):
-                        query="SELECT COUNT(guide_id) AS count from guided_tour WHERE tour_date=%s AND slot=%s AND Guide_id=%s GROUP BY tour_date,slot,Guide_id"
-                        cur.execute(query,(d1,s,option))
-                        list1=cur.fetchone() 
-                        if list1:   
-                            ng=list1['count']
-                        else:
-                            ng=0
+                    if d1>=current_date:
+                        if st.button("Book"):
+                            query="SELECT COUNT(guide_id) AS count from guided_tour WHERE tour_date=%s AND slot=%s AND Guide_id=%s GROUP BY tour_date,slot,Guide_id"
+                            cur.execute(query,(d1,s,option))
+                            list1=cur.fetchone() 
+                            if list1:   
+                                ng=list1['count']
+                            else:
+                                ng=0
 
-                        if 15>=ng:
-                            query2 = "SELECT user_id FROM login WHERE username=%s"
-                            cur.execute(query2, (username,))
-                            result_user = cur.fetchone()
+                            if 15>=ng:
+                                query2 = "SELECT user_id FROM login WHERE username=%s"
+                                cur.execute(query2, (username,))
+                                result_user = cur.fetchone()
 
-                            query1="INSERT INTO guided_tour(guide_id,tour_date,booking_user_id,slot) VALUES (%s,%s,%s,%s)"
-                            try:
-                                cur.execute(query1,(option,d1,result_user['user_id'],s[0]))
-                                st.write("Tour with Guide successfully !!")
-                                connection.commit()
+                                query1="INSERT INTO guided_tour(guide_id,tour_date,booking_user_id,slot) VALUES (%s,%s,%s,%s)"
+                                try:
+                                    cur.execute(query1,(option,d1,result_user['user_id'],s[0]))
+                                    st.write("Tour with Guide successfully !!")
+                                    connection.commit()
 
-                            except mysql.connector.Error as e:
-                                st.error(f"Error: {e}")
-                        else:
-                            st.write("No slot available for the given booking date!!")
+                                except mysql.connector.Error as e:
+                                    st.error(f"Error: {e}")
+                            else:
+                                st.write("No slot available for the given booking date!!")
+                            
                         
-                    
-                        query2='''SELECT gt.tour_id,gt.guide_id,g.Name,g.phone_number,gt.tour_date 
-                        FROM guided_tour gt JOIN guide g ON gt.Guide_id=g.Guide_id 
-                        WHERE booking_user_id=(SELECT user_id from login where username=%s) AND g.Guide_id=%s'''
-                        cur.execute(query2,(username,option))
-                        list4=cur.fetchall()
-                        print(list4)
-                        connection.close()
+                            query2='''SELECT gt.tour_id,gt.guide_id,g.Name,g.phone_number,gt.tour_date 
+                            FROM guided_tour gt JOIN guide g ON gt.Guide_id=g.Guide_id 
+                            WHERE booking_user_id=(SELECT user_id from login where username=%s) AND g.Guide_id=%s'''
+                            cur.execute(query2,(username,option))
+                            list4=cur.fetchall()
+                            print(list4)
+                            connection.close()
 
-                        st.table(list4)
-                        
+                            st.table(list4)
+                
+                    else:
+                        st.warning("select correct date")
+
+                            
             except mysql.connector.Error as e:
                 st.error(f"Error: {e}")
                         
--- AGMS_Modules/Ticket_book.py
+++ AGMS_Modules/Ticket_book.py
@@ -13,71 +13,75 @@ from datetime import datetime  # Updated import statement
 def do_ticket_book(username):
     connection = mysql.connector.connect(
         host='localhost', username='root', password='wasd', database='agms2')
+    
     try:
         if connection.is_connected():
             cur = connection.cursor(dictionary=True)
+            
             try:
                 s1 = st.date_input("Enter booking date: ")
                 s1 = s1.strftime('%Y-%m-%d')
-                nt = st.number_input(
-                    'Enter the no of tickets: ', step=1, min_value=1)
+                
+                # Check if the selected date is greater than or equal to the current date
+                if s1 >= datetime.now().strftime('%Y-%m-%d'):
+                    nt = st.number_input('Enter the no of tickets: ', step=1, min_value=1)
 
-                if st.button("Book"):
-                    try:
-                        query = "SELECT COUNT(*) from ticket WHERE date_of_visit=%s"
-                        cur.execute(query, (s1,))
-                        result_tickets = cur.fetchone()
-                        available_tickets = 200 - result_tickets['COUNT(*)']
+                    if st.button("Book"):
+                        try:
+                            query = "SELECT COUNT(*) from ticket WHERE date_of_visit=%s"
+                            cur.execute(query, (s1,))
+                            result_tickets = cur.fetchone()
+                            available_tickets = 200 - result_tickets['COUNT(*)']
 
-                        if available_tickets >= nt:
-                            query2 = "SELECT user_id FROM login WHERE username=%s"
-                            cur.execute(query2, (username,))
-                            result_user = cur.fetchone()
+                            if available_tickets >= nt:
+                                query2 = "SELECT user_id FROM login WHERE username=%s"
+                                cur.execute(query2, (username,))
+                                result_user = cur.fetchone()
 
-                            if result_user:
-                                uid = result_user['user_id']
+                                if result_user:
+                                    uid = result_user['user_id']
 
-                                for i in range(nt):
-                                    query1 = "INSERT INTO ticket(date_of_visit,booking_date,booking_user_id) VALUES (%s,%s,%s)"
+                                    for i in range(nt):
+                                        query1 = "INSERT INTO ticket(date_of_visit,booking_date,booking_user_id) VALUES (%s,%s,%s)"
+                                        cur.execute(
+                                            query1, (s1, datetime.now(), uid))
+                                        connection.commit()
+
+                                    query3 = '''SELECT booking_user_id, date_of_visit, booking_date, COUNT(*) AS No_of_tickets
+                                                FROM ticket
+                                                WHERE date_of_visit=%s AND booking_user_id=%s
+                                                GROUP BY booking_user_id, date_of_visit, booking_date'''
+
+                                    st.success("Ticket Booked Successfully")
+                                    cur.execute(query3, (s1, uid))
+                                    result_tickets_user = cur.fetchall()
+
+                                    st.table(result_tickets_user)
+
+                                    current_timestamp = datetime.now()
+                                    current_timestamp = current_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+                                    query4 = "INSERT INTO visitor_transactions(booking_user_id, No_of_tickets, time) VALUES (%s,%s,%s)"
                                     cur.execute(
-                                        query1, (s1, datetime.now(), uid))
+                                        query4, (uid, result_tickets_user[0]['No_of_tickets'], current_timestamp))
                                     connection.commit()
 
-                                query3 = '''SELECT booking_user_id, date_of_visit, booking_date, COUNT(*) AS No_of_tickets
-                                            FROM ticket
-                                            WHERE date_of_visit=%s AND booking_user_id=%s
-                                            GROUP BY booking_user_id, date_of_visit, booking_date'''
-
-                                st.success("Ticket Booked Successfully")
-                                cur.execute(query3, (s1, uid))
-                                result_tickets_user = cur.fetchall()
-
-                                st.table(result_tickets_user)
-                                # df1 = pd.DataFrame(result_tickets_user, columns=['User ID', 'Tour Date', 'Ticket Booking date', 'No of Tickets'])
-                                # st.header("Ticket Booked details")
-                                # st.write(df1)
-
-                                current_timestamp = datetime.now()
-                                current_timestamp = current_timestamp.strftime(
-                                    "%Y-%m-%d %H:%M:%S")
-
-                                query4 = "INSERT INTO visitor_transactions(booking_user_id, No_of_tickets, time) VALUES (%s,%s,%s)"
-                                cur.execute(
-                                    query4, (uid, result_tickets_user[0]['No_of_tickets'], current_timestamp))
-                                connection.commit()
+                                else:
+                                    st.write("User not found.")
 
                             else:
-                                st.write("User not found.")
+                                st.write("No tickets available for the given booking date!!")
 
-                        else:
-                            st.write(
-                                "No tickets available for the given booking date!!")
-
-                    except mysql.connector.Error as e:
-                        st.error(f"Error: {e}")
+                        except mysql.connector.Error as e:
+                            st.error(f"Error: {e}")
+                else:
+                    st.write("Please select a date greater than or equal to the current date.")
 
             except mysql.connector.Error as e:
                 st.error(f"Error: {e}")
+
+    except mysql.connector.Error as e:
+        st.error(f"Error: {e}")
 
     except mysql.connector.Error as e:
         st.error(f"Error: {e}")
